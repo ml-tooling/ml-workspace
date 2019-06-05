040bf210eb4b0c47637114bd75d1458bd9c48404
Lukas Masuch <lukas.masuch@gmail.com>
2019-06-06 00:15:31 +0200

    Added shebangs to scripts

--- docker-res/scripts/check_xfdesktop_leak.py
+++ docker-res/scripts/check_xfdesktop_leak.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+
 # System libraries
 from __future__ import absolute_import, division, print_function
 
--- docker-res/scripts/run_tools.py
+++ docker-res/scripts/run_tools.py
@@ -1,4 +1,4 @@
-# !/usr/bin/python
+#!/usr/bin/python
 
 """
 Configure and run tools
