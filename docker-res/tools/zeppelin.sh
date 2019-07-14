38f290d93a954a5dd415a7f78c0e86a7cbba0b70
Lukas Masuch <lukas.masuch@gmail.com>
2019-07-14 19:47:56 +0200

    Add zeppelin installer

---
+++ docker-res/tools/zeppelin.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+if [ -z "$1" ]; then
+    echo "A port needs to be provided as argument"
+    exit 1
+fi
+
+if [ ! -f /resources/zeppelin/zeppelin-0.8.1-bin-all/bin/zeppelin-daemon.sh  ]; then
+    echo "Installing zeppelin"
+    mkdir /resources/zeppelin
+    cd /resources/zeppelin
+    wget https://www.apache.org/dist/zeppelin/zeppelin-0.8.1/zeppelin-0.8.1-bin-all.tgz -O ./zeppelin-0.8.1-bin-all.tgz
+    tar -zxvf zeppelin-0.8.1-bin-all.tgz
+    rm zeppelin-0.8.1-bin-all.tgz
+fi
+
+# Run
+echo "Starting zeppelin on port "$1
+mkdir $WORKSPACE_HOME/zeppelin
+export ZEPPELIN_NOTEBOOK_DIR=$WORKSPACE_HOME/zeppelin
+export ZEPPELIN_PORT=$1
+/resources/zeppelin/zeppelin-0.8.1-bin-all/bin/zeppelin-daemon.sh start
