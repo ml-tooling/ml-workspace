bff48ce130173f1426d686d59ecb7afd4ef62612
Lukas Masuch <lukas.masuch@gmail.com>
2019-07-14 20:16:15 +0200

    Add francise installer

---
+++ docker-res/tools/franchise.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+if [ -z "$1" ]; then
+    echo "A port needs to be provided as argument"
+    exit 1
+fi
+
+if [ ! -d "/resources/franchise" ]; then
+    cd /resources
+    echo "Installing franchise on port "$1
+    npm update
+    git clone --depth 1 https://github.com/HVF/franchise.git
+    cd franchise
+    sed -i 's/"start": "nwb serve-react-app"/"start": "nwb serve-react-app --port='"$1"'"/g' package.json
+    npm install
+fi
+
+# Run
+echo "Starting franchise on port "$1
+# TODO change port again via sed and regex
+cd /resources/franchise
+npm start
