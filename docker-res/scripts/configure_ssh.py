f548ad35f692af08116fcbb3d4eca9b319d5e404
Lukas Masuch <lukas.masuch@gmail.com>
2019-07-24 04:10:25 +0200

    Surpress stdout

--- docker-res/scripts/configure_ssh.py
+++ docker-res/scripts/configure_ssh.py
@@ -18,10 +18,10 @@ SSH_KEY_NAME = "id_ed25519" # use default name instead of workspace_key
 # TODO add container and user information as a coment via -C
 if not os.path.exists("~/.ssh/"+SSH_KEY_NAME):
     # create ssh key if it does not exist yet
-    call("ssh-keygen -f ~/.ssh/{} -t ed25519 -q -N \"\"".format(SSH_KEY_NAME), shell=True)
+    call("ssh-keygen -f ~/.ssh/{} -t ed25519 -q -N \"\" > /dev/null".format(SSH_KEY_NAME), shell=True)
 call("chmod 600 ~/.ssh/{}".format(SSH_KEY_NAME), shell=True)
 # echo "" >> ~/.ssh/authorized_keys will prepend a new line before the key is added to the file
 call("echo "" >> ~/.ssh/authorized_keys && cat ~/.ssh/{}.pub | tee -a ~/.ssh/authorized_keys".format(SSH_KEY_NAME), shell=True)
 # Add identity to ssh agent -> e.g. can be used for git authorization
-call("eval \"$(ssh-agent -s)\" && ssh-add ~/.ssh/"+SSH_KEY_NAME, shell=True)
+call("eval \"$(ssh-agent -s)\" && ssh-add ~/.ssh/"+SSH_KEY_NAME + " > /dev/null", shell=True)
 ###
