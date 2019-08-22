6be829ac7cb0fa2c61b42cb367381ec014b293a1
Lukas Masuch <lukas.masuch@gmail.com>
2019-08-22 16:30:00 +0200

    Reformatted ml-service example

--- docker-res/tests/ml-service/__main__.py
+++ docker-res/tests/ml-service/__main__.py
@@ -4,13 +4,16 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
+
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
+
 
 def patch_fastapi(app: FastAPI):
     """
@@ -23,20 +26,20 @@ def patch_fastapi(app: FastAPI):
 
     async def swagger_ui_html(req: Request) -> HTMLResponse:
         swagger_ui = get_swagger_ui_html(
-                    openapi_url="./openapi.json",
-                    title=app.title + " - Swagger UI",
-                    oauth2_redirect_url=app.swagger_ui_oauth2_redirect_url,
-                )
+            openapi_url="./"+app.openapi_url.lstrip("/"),
+            title=app.title + " - Swagger UI",
+            oauth2_redirect_url=app.swagger_ui_oauth2_redirect_url,
+        )
 
         # insert request interceptor to have all request run on relativ path
         request_interceptor = 'requestInterceptor: (e)  => {' \
-                          '\n\t\t\tvar url = window.location.origin + window.location.pathname' \
-                          '\n\t\t\turl = url.substring( 0, url.lastIndexOf( "/" ) + 1);' \
-                          '\n\t\t\turl = e.url.replace(/https:\/\/[^/]*\//i, url);' \
-                          '\n\t\t\te.contextUrl = url' \
-                          '\n\t\t\te.url = url' \
-                          '\n\t\t\treturn e;}'
-    
+            '\n\t\t\tvar url = window.location.origin + window.location.pathname' \
+            '\n\t\t\turl = url.substring( 0, url.lastIndexOf( "/" ) + 1);' \
+            '\n\t\t\turl = e.url.replace(/https:\/\/[^/]*\//i, url);' \
+            '\n\t\t\te.contextUrl = url' \
+            '\n\t\t\te.url = url' \
+            '\n\t\t\treturn e;}'
+
         return HTMLResponse(swagger_ui.body.decode("utf-8").replace("deepLinking: true", "deepLinking: true,\n\t\t" + request_interceptor))
 
     # remove old docs route and add our patched route
@@ -48,6 +51,7 @@ def patch_fastapi(app: FastAPI):
 
     app.router.routes = routes_new
     app.add_route(app.docs_url, swagger_ui_html, include_in_schema=False)
+
 
 patch_fastapi(app)
 
