982c69beeb977b30d2d11fdb35044f1a7e04f964
Lukas Masuch <lukas.masuch@gmail.com>
2019-08-23 19:46:43 +0200

    Update gpu workspace build

--- gpu-flavor/build.py
+++ gpu-flavor/build.py
@@ -1,15 +1,10 @@
-
-"""
-Build workspace gpu image
-"""
-
 import os, sys
 import subprocess
 import argparse
 import datetime
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--name', help='name of docker container', default="ml-workspace-gpu")
+parser.add_argument('--name', help='base name of docker container', default="ml-workspace")
 parser.add_argument('--version', help='version tag of docker container', default="latest")
 parser.add_argument('--deploy', help='deploy docker container to remote', action='store_true')
 parser.add_argument('--flavor', help='flavor (gpu) used for docker container', default='gpu')
@@ -26,7 +21,12 @@ def call(command):
     return subprocess.call(command, shell=True)
 
 # calls build scripts in every module with same flags
-def build(module):
+def build(module="."):
+    
+    if not os.path.isdir(module):
+        print("Could not find directory for " + module)
+        sys.exit(1)
+    
     build_command = "python build.py"
 
     if args.version:
@@ -36,7 +36,7 @@ def build(module):
         build_command += " --deploy"
  
     if args.flavor:
-        build_command += " --flavor"
+        build_command += " --flavor=" + str(args.flavor)
 
     working_dir = os.path.dirname(os.path.realpath(__file__))
     full_command = "cd " + module + " && " + build_command + " && cd " + working_dir
@@ -46,15 +46,28 @@ def build(module):
         print("Failed to build module " + module)
         sys.exit(1)
 
+if not args.flavor:
+    args.flavor = "gpu"
+
+args.flavor = str(args.flavor).lower()
+
+if args.flavor == "all":
+    args.flavor = "gpu"
+    build()
+    sys.exit(0)
+
 # unknown flavor -> try to build from subdirectory
 if args.flavor not in ["gpu"]:
     # assume that flavor has its own directory with build.py
-    build(args.flavor)
+    build(args.flavor + "-flavor")
     sys.exit(0)
 
 service_name = os.path.basename(os.path.dirname(os.path.realpath(__file__)))
 if args.name:
     service_name = args.name
+
+# add flavor to service name
+service_name += "-" + args.flavor
 
 # docker build
 git_rev = "unknown"
@@ -69,14 +82,15 @@ try:
 except:
     pass
 
-vcs_ref_build_arg = " --build-arg VCS_REF=" + str(git_rev)
-build_date_build_arg = " --build-arg BUILD_DATE=" + str(build_date)
-flavor_build_arg = " --build-arg WORKSPACE_FLAVOR=" + str(args.flavor)
-version_build_arg = " --build-arg WORKSPACE_VERSION=" + str(args.version)
+vcs_ref_build_arg = " --build-arg ARG_VCS_REF=" + str(git_rev)
+build_date_build_arg = " --build-arg ARG_BUILD_DATE=" + str(build_date)
+flavor_build_arg = " --build-arg ARG_WORKSPACE_FLAVOR=" + str(args.flavor)
+version_build_arg = " --build-arg ARG_WORKSPACE_VERSION=" + str(args.version)
 
 versioned_image = service_name+":"+str(args.version)
 latest_image = service_name+":latest"
-failed = call("docker build -t " + versioned_image + " -t " + latest_image + " " + version_build_arg + " " + flavor_build_arg + " ./")
+failed = call("docker build -t "+ versioned_image + " -t " + latest_image + " " 
+            + version_build_arg + " " + flavor_build_arg+ " " + vcs_ref_build_arg + " " + build_date_build_arg + " ./")
 
 if failed:
     print("Failed to build container")
