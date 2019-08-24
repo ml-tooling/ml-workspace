e3e2d043b098620e9b3e2665ebb6e04b206e916f
Lukas Masuch <lukas.masuch@gmail.com>
2019-08-24 18:46:17 +0200

    Add new tools (docker-client, kubernetes-client, theia-ide)

---
+++ docker-res/tools/docker-client.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+# Stops script execution if a command has an error
+set -e
+
+INSTALL_ONLY=0
+# Loop through arguments and process them: https://pretzelhands.com/posts/command-line-flags
+for arg in "$@"; do
+    case $arg in
+        -i|--install) INSTALL_ONLY=1 ; shift ;;
+        *) break ;;
+    esac
+done
+
+if ! hash docker 2>/dev/null; then
+    echo "Installing Docker Client"
+    mkdir -p $RESOURCES_PATH"/docker"
+    cd $RESOURCES_PATH"/docker"
+    wget --quiet https://get.docker.com/builds/Linux/x86_64/docker-latest.tgz -O ./docker.tar.gz
+    tar xfz ./docker.tar.gz
+    rm -rf ./docker.tar.gz
+    # TODO? only move the docker client to bin
+    mv ./docker/docker /usr/bin
+    chmod a+rwx /usr/bin/docker
+    cd $RESOURCES_PATH
+    rm -rf ./docker
+    curl -L "https://raw.githubusercontent.com/MartinsThiago/rdocker/master/rdocker.sh" > /usr/local/bin/rdocker &&\
+    chmod a+rwx /usr/local/bin/rdocker &&\
+    # Todo install docker compose
+    # curl  -L "https://github.com/docker/compose/releases/download/1.7.1/docker-compose-`uname -s`-`uname -m`" > /usr/local/bin/docker-compose &&\
+    # chmod +x /usr/local/bin/docker-compose
+    pip install --no-cache-dir docker
+else
+    echo "Docker Client is already installed"
+fi
+
+# Run
+if [ $INSTALL_ONLY = 0 ] ; then
+    echo "Use Docker Client via command line:"
+    docker --help
+    sleep 20
+fi
