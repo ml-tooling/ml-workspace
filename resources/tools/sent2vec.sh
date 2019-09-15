248dd22b98d2000f6da2968c4e971a0f3cf4ae2c
Lukas Masuch <lukas.masuch@gmail.com>
2019-09-15 21:08:09 +0200

    Add sent2vec installer script

---
+++ resources/tools/sent2vec.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# Stops script execution if a command has an error
+set -e
+
+INSTALL_ONLY=0
+# Loop through arguments and process them: https://pretzelhands.com/posts/command-line-flags
+for arg in "$@"; do
+    case $arg in
+        -i|--install) INSTALL_ONLY=1 ; shift ;;
+        *) break ;;
+    esac
+done
+
+#https://github.com/epfml/sent2vec
+echo "Installing Sent2vec. Please wait..."
+pip install -U --no-cache-dir git+https://github.com/epfml/sent2vec
+
+echo "User sent2vec via python as described here: https://github.com/epfml/sent2vec#directly-from-python"
