19292fefcdcf5e5378fcba3b9cb9395327cb2021
Lukas Masuch <lukas.masuch@gmail.com>
2019-09-19 20:12:14 +0200

    Improve zsh shell installer

--- resources/tools/oh-my-zsh.sh
+++ resources/tools/oh-my-zsh.sh
@@ -16,14 +16,30 @@ if ! hash zsh 2>/dev/null; then
     echo "Installing Oh My ZSH. Please wait..."
     apt-get update
     apt-get install --yes zsh
+    # required for lots of themes
+    apt-get install -y fonts-powerline
     yes | sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
     git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
     git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
-    printf "export ZSH=\"/root/.oh-my-zsh\"\nZSH_THEME=\"robbyrussell\"\nplugins=(git colorize extract pip zsh-autosuggestions history-substring-search zsh-syntax-highlighting)\nsource \$ZSH/oh-my-zsh.sh" > ~/.zshrc 
+    git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
+    git clone https://github.com/supercrabtree/k ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/k
+    git clone https://github.com/chrissicool/zsh-256color ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-256color
+    printf "export ZSH=\"/root/.oh-my-zsh\"\nZSH_THEME=\"avit\"\nZSH_AUTOSUGGEST_HIGHLIGHT_STYLE=\"fg=245\"\nplugins=(git k extract colorize pip npm zsh-256color supervisor command-not-found autojump colored-man-pages git-flow git-extras httpie python zsh-autosuggestions history-substring-search zsh-completions zsh-syntax-highlighting)\nsource \$ZSH/oh-my-zsh.sh" > ~/.zshrc 
+    # TODO add z
+    # Other good themes: avit, sorin, clean
 else
     echo "ZSH is already installed"
 fi
+
+# docker, kubectl
+
+    # TODO install zsh completions?
+    # sudo sh -c "echo 'deb http://download.opensuse.org/repositories/shells:/zsh-users:/zsh-completions/xUbuntu_16.04/ /' > /etc/apt/sources.list.d/shells:zsh-users:zsh-completions.list"
+    # wget -nv https://download.opensuse.org/repositories/shells:zsh-users:zsh-completions/xUbuntu_16.04/Release.key -O Release.key
+    # sudo apt-key add - < Release.key
+    # sudo apt-get update
+    # sudo apt-get install zsh-completions
 
 # Run
 if [ $INSTALL_ONLY = 0 ] ; then
