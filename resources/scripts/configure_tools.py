954f7e90895c46509b26c4e3248d6a1f1d1ff247
Lukas Masuch <lukas.masuch@gmail.com>
2019-09-27 18:51:13 +0200

    Remove base url from jupyter/jupyterlab shortcuts

--- resources/scripts/configure_tools.py
+++ resources/scripts/configure_tools.py
@@ -19,14 +19,7 @@ log = logging.getLogger(__name__)
 
 ENV_RESOURCES_PATH = os.getenv("RESOURCES_PATH", "/resources")
 ENV_WORKSPACE_HOME = os.getenv("WORKSPACE_HOME", "/workspace")
-ENV_WORKSPACE_BASE_URL = os.getenv("WORKSPACE_BASE_URL", "/")
 HOME = os.getenv("HOME", "/root")
-
-if not ENV_WORKSPACE_BASE_URL.startswith("/"):
-    ENV_WORKSPACE_BASE_URL = "/" + ENV_WORKSPACE_BASE_URL
-
-if not ENV_WORKSPACE_BASE_URL.endswith("/"):
-    ENV_WORKSPACE_BASE_URL = ENV_WORKSPACE_BASE_URL + "/"
 
 DESKTOP_PATH = HOME + "/Desktop"
 
@@ -42,17 +35,16 @@ if ENV_AUTHENTICATE_VIA_JUPYTER.lower() == "true":
 elif ENV_AUTHENTICATE_VIA_JUPYTER and ENV_AUTHENTICATE_VIA_JUPYTER.lower() != "false":
     token_parameter = "?token=" + ENV_AUTHENTICATE_VIA_JUPYTER
 
-# Create Jupyter Shortcut
-url = 'http://localhost:8092' + ENV_WORKSPACE_BASE_URL + token_parameter
+# Create Jupyter Shortcut - at runtime since the jupyterhub token is needed
+url = 'http://localhost:8092' + token_parameter
 shortcut_metadata = '[Desktop Entry]\nVersion=1.0\nType=Link\nName=Jupyter Notebook\nComment=\nCategories=Development;\nIcon=' + ENV_RESOURCES_PATH + '/icons/jupyter-icon.png\nURL=' + url
-
 call('printf "' + shortcut_metadata + '" > ' + DESKTOP_PATH + '/jupyter.desktop', shell=True) # create a link on the Desktop to your Jupyter notebook server
 call('chmod +x ' + DESKTOP_PATH + '/jupyter.desktop', shell=True) # Make executable
 call('printf "' + shortcut_metadata + '" > /usr/share/applications/jupyter.desktop', shell=True) # create a link in categories menu to your Jupyter notebook server
 call('chmod +x /usr/share/applications/jupyter.desktop', shell=True) # Make executable
 
 # Create Jupyter Lab Shortcut
-url = 'http://localhost:8092' + ENV_WORKSPACE_BASE_URL + "lab" + token_parameter
+url = 'http://localhost:8092' + "/lab" + token_parameter
 shortcut_metadata = '[Desktop Entry]\nVersion=1.0\nType=Link\nName=Jupyter Lab\nComment=\nCategories=Development;\nIcon=' + ENV_RESOURCES_PATH + '/icons/jupyterlab-icon.png\nURL=' + url
 
 call('printf "' + shortcut_metadata + '" > /usr/share/applications/jupyterlab.desktop', shell=True) # create a link in categories menu to your Jupyter Lab server
