956efc4ca63704b0a5ad4cbf1d45d569ef658f24
Lukas Masuch <lukas.masuch@gmail.com>
2019-10-01 12:09:46 +0200

    Remove Jupyterhub base url for base url construction

--- resources/docker-entrypoint.py
+++ resources/docker-entrypoint.py
@@ -27,7 +27,7 @@ def set_env_variable(env_variable: str, value: str):
     os.environ[env_variable] = value
 
 # Manage base path dynamically
-ENV_JUPYTERHUB_BASE_URL = os.getenv("JUPYTERHUB_BASE_URL")
+
 ENV_JUPYTERHUB_SERVICE_PREFIX = os.getenv("JUPYTERHUB_SERVICE_PREFIX")
 
 ENV_NAME_WORKSPACE_BASE_URL = "WORKSPACE_BASE_URL"
@@ -36,9 +36,13 @@ base_url = os.environ[ENV_NAME_WORKSPACE_BASE_URL]
 if not base_url:
     base_url = ""
 
-if ENV_JUPYTERHUB_BASE_URL and ENV_JUPYTERHUB_SERVICE_PREFIX:
-    # Installation with Jupyterhub -> use combination as base path
-    base_url = ENV_JUPYTERHUB_BASE_URL.rstrip('/') + ENV_JUPYTERHUB_SERVICE_PREFIX
+if ENV_JUPYTERHUB_SERVICE_PREFIX:
+    # Installation with Jupyterhub
+    
+    # Base Url is not needed, Service prefix contains full path
+    # ENV_JUPYTERHUB_BASE_URL = os.getenv("JUPYTERHUB_BASE_URL")
+    # ENV_JUPYTERHUB_BASE_URL.rstrip('/') + 
+    base_url = ENV_JUPYTERHUB_SERVICE_PREFIX
 
 # Add leading slash
 if not base_url.startswith("/"):
