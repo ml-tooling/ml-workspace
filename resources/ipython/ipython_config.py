41234c87b255a39c7f29e9d9e2606c151b52d8bb
Manu C <cupcicm@gmail.com>
2020-01-10 23:17:26 +0100

    Save all ipython session inputs.
    Change the ipython config so that each command is logged by default.

--- resources/ipython/ipython_config.py
+++ resources/ipython/ipython_config.py
@@ -1,4 +1,18 @@
-# Make matplotlib output in Jupyter notebooks display correctly
+from time import strftime
+import os
+
+def configure_logging(c):
+    folder = os.environ('LOGS_PATH') or '/logs'
+    logfilename = strftime('ipython_log_%Y-%m-%d')+".py"
+    logfilepath = "/%s" % (folder,logfilename)
+
+    c.LoggingMagics.quiet = True
+    c.TerminalInteractiveShell.logappend = logfilepath
+    c.TerminalInteractiveShell.logstart = True
+
 c = get_config()
 
+# Make matplotlib output in Jupyter notebooks display correctly
 c.IPKernelApp.matplotlib = 'inline'
+
+configure_logging(c)
