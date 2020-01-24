3ea01c9ba85e9370225a88ce7fb9b96ff7ea2232
Lukas Masuch <lukas.masuch@gmail.com>
2020-01-24 15:54:14 +0100

    Update tool installer

--- resources/tools/vs-code-server.sh
+++ resources/tools/vs-code-server.sh
@@ -17,6 +17,10 @@ done
 if [ ! -f "/usr/local/bin/code-server"  ]; then
     echo "Installing VS Code Server. Please wait..."
     cd ${RESOURCES_PATH}
+    # CODE_SERVER_VERSION=2.1698
+    # VS_CODE_VERSION=$CODE_SERVER_VERSION-vsc1.41.1
+    # wget -q https://github.com/cdr/code-server/releases/download/$CODE_SERVER_VERSION/code-server$VS_CODE_VERSION-linux-x86_64.tar.gz -O ./vscode-web.tar.gz
+    # Use older version, since newer has some problems with python extension
     VS_CODE_VERSION=2.1692-vsc1.39.2
     wget -q https://github.com/cdr/code-server/releases/download/$VS_CODE_VERSION/code-server$VS_CODE_VERSION-linux-x86_64.tar.gz -O ./vscode-web.tar.gz
     tar xfz ./vscode-web.tar.gz
