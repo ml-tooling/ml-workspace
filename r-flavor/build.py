07b39d1d94e41a9ea665837a891a75262af8863e
Benjamin Raethlein <benjamin.raethlein@gmail.com>
2020-11-16 16:57:42 +0100

    Use universal_build lib for build.py

--- r-flavor/build.py
+++ r-flavor/build.py
@@ -1,110 +1,88 @@
-import os, sys
 import subprocess
 import argparse
 import datetime
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--name', help='base name of docker container', default="ml-workspace")
-parser.add_argument('--version', help='version tag of docker container', default="latest")
-parser.add_argument('--deploy', help='deploy docker container to remote', action='store_true')
-parser.add_argument('--flavor', help='flavor (r) used for docker container', default='r')
+from universal_build import build_utils
+
+parser = argparse.ArgumentParser(add_help=False)
+parser.add_argument(
+    "--flavor",
+    help="flavor (full, light, minimal) used for docker container",
+    default="r",
+)
 
 REMOTE_IMAGE_PREFIX = "mltooling/"
+FLAG_FLAVOR = "flavor"
+COMPONENT_NAME = "r-flavor"
 
-args, unknown = parser.parse_known_args()
-if unknown:
-    print("Unknown arguments "+str(unknown))
+args = build_utils.get_sanitized_arguments(argument_parser=parser)
 
-# Wrapper to print out command
-def call(command):
-    print("Executing: "+command)
-    return subprocess.call(command, shell=True)
+if not args[FLAG_FLAVOR]:
+    args[FLAG_FLAVOR] = "r"
 
-# calls build scripts in every module with same flags
-def build(module="."):
-    
-    if not os.path.isdir(module):
-        print("Could not find directory for " + module)
-        sys.exit(1)
-    
-    build_command = "python build.py"
+args[FLAG_FLAVOR] = str(args[FLAG_FLAVOR]).lower()
 
-    if args.version:
-        build_command += " --version=" + str(args.version)
-
-    if args.deploy:
-        build_command += " --deploy"
- 
-    if args.flavor:
-        build_command += " --flavor=" + str(args.flavor)
-
-    working_dir = os.path.dirname(os.path.realpath(__file__))
-    full_command = "cd " + module + " && " + build_command + " && cd " + working_dir
-    print("Building " + module + " with: " + full_command)
-    failed = call(full_command)
-    if failed:
-        print("Failed to build module " + module)
-        sys.exit(1)
-
-if not args.flavor:
-    args.flavor = "r"
-
-args.flavor = str(args.flavor).lower()
-
-if args.flavor == "all":
-    args.flavor = "r"
-    build()
-    sys.exit(0)
+if args[FLAG_FLAVOR] == "all":
+    args[FLAG_FLAVOR] = "r"
+    build_utils.build(".", args)
+    build_utils.exit_process(0)
 
 # unknown flavor -> try to build from subdirectory
-if args.flavor not in ["r"]:
+if args[FLAG_FLAVOR] not in ["r"]:
     # assume that flavor has its own directory with build.py
-    build(args.flavor + "-flavor")
-    sys.exit(0)
+    build_utils.build(args[FLAG_FLAVOR] + "-flavor", args)
+    build_utils.exit_process(0)
 
-service_name = os.path.basename(os.path.dirname(os.path.realpath(__file__)))
-if args.name:
-    service_name = args.name
-
-# add flavor to service name
-service_name += "-" + args.flavor
+service_name = COMPONENT_NAME + "-" + args[FLAG_FLAVOR]
 
 # docker build
 git_rev = "unknown"
 try:
-    git_rev = subprocess.check_output(["git", "rev-parse", "--short", "HEAD"]).decode('ascii').strip()
-except:
+    git_rev = (
+        subprocess.check_output(["git", "rev-parse", "--short", "HEAD"])
+        .decode("ascii")
+        .strip()
+    )
+except Exception:
     pass
 
 build_date = datetime.datetime.utcnow().isoformat("T") + "Z"
 try:
-    build_date = subprocess.check_output(['date', '-u', '+%Y-%m-%dT%H:%M:%SZ']).decode('ascii').strip()
-except:
+    build_date = (
+        subprocess.check_output(["date", "-u", "+%Y-%m-%dT%H:%M:%SZ"])
+        .decode("ascii")
+        .strip()
+    )
+except Exception:
     pass
 
 vcs_ref_build_arg = " --build-arg ARG_VCS_REF=" + str(git_rev)
 build_date_build_arg = " --build-arg ARG_BUILD_DATE=" + str(build_date)
-flavor_build_arg = " --build-arg ARG_WORKSPACE_FLAVOR=" + str(args.flavor)
-version_build_arg = " --build-arg ARG_WORKSPACE_VERSION=" + str(args.version)
+flavor_build_arg = " --build-arg ARG_WORKSPACE_FLAVOR=" + str(args[FLAG_FLAVOR])
+version_build_arg = " --build-arg ARG_WORKSPACE_VERSION=" + str(
+    args[build_utils.FLAG_VERSION]
+)
 
-versioned_image = service_name+":"+str(args.version)
-latest_image = service_name+":latest"
-failed = call("docker build -t "+ versioned_image + " -t " + latest_image + " " 
-            + version_build_arg + " " + flavor_build_arg+ " " + vcs_ref_build_arg + " " + build_date_build_arg + " ./")
+if args[build_utils.FLAG_MAKE]:
+    build_args = (
+        version_build_arg
+        + " "
+        + flavor_build_arg
+        + " "
+        + vcs_ref_build_arg
+        + " "
+        + build_date_build_arg
+    )
 
-if failed:
-    print("Failed to build container")
-    sys.exit(1)
+    completed_process = build_utils.build_docker_image(
+        COMPONENT_NAME, version=args[build_utils.FLAG_VERSION], build_args=build_args
+    )
+    if completed_process.returncode > 0:
+        build_utils.exit_process(1)
 
-remote_versioned_image = REMOTE_IMAGE_PREFIX + versioned_image
-call("docker tag " + versioned_image + " " + remote_versioned_image)
-
-remote_latest_image = REMOTE_IMAGE_PREFIX + latest_image
-call("docker tag " + latest_image + " " + remote_latest_image)
-
-if args.deploy:
-    call("docker push " + remote_versioned_image)
-
-    if "SNAPSHOT" not in args.version:
-    # do not push SNAPSHOT builds as latest version
-        call("docker push " + remote_latest_image)
+if args[build_utils.FLAG_RELEASE]:
+    build_utils.release_docker_image(
+        COMPONENT_NAME,
+        args[build_utils.FLAG_VERSION],
+        args[build_utils.FLAG_DOCKER_IMAGE_PREFIX],
+    )
