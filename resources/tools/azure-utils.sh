f820578d4a59124433f9ddf8f9aef57993727f53
Jan Kalkan <jan.kalkan@mailbox.org>
2020-11-30 22:24:05 +0100

    Update tool installer scripts

--- resources/tools/azure-utils.sh
+++ resources/tools/azure-utils.sh
@@ -14,15 +14,18 @@ done
 
 echo "Installing Azure Tooling Collection. Please wait..."
 pip install --no-cache-dir \
-        azure \
+        azure-mgmt-compute \
+        azure-mgmt-storage \
+        azure-mgmt-resource \
+        azure-keyvault-secrets \
+        azure-storage-blob \
         msrestazure \
         azure-mgmt-resource \
         azure-mgmt-datalake-store
 
-# Install vscode azure extension 
+# Install vscode azure extension
 if hash code 2>/dev/null; then
     # https://marketplace.visualstudio.com/items?itemName=ms-vscode.vscode-node-azure-pack
     LD_LIBRARY_PATH="" LD_PRELOAD="" code --user-data-dir=$HOME/.config/Code/ --extensions-dir=$HOME/.vscode/extensions/ --install-extension ms-vscode.vscode-node-azure-pack
 else
-    echo "Please install the desktop version of vscode via the vs-code-desktop.sh script to install azure vscode extensions."
-fi
+    echo "Please install the desktop version of vscode via the vs-code-desktop.sh script to install azure vscode extensions.
