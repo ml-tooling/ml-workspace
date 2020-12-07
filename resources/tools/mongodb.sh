eb623da9e97e5a2acca6b0dbab36cf8c04994c73
Lukas Masuch <lukas.masuch@gmail.com>
2020-12-07 11:37:07 +0100

    Update mongodb installer

--- resources/tools/mongodb.sh
+++ resources/tools/mongodb.sh
@@ -15,15 +15,16 @@ done
 if ! hash mongod 2>/dev/null; then
     echo "Installing MongoDB. Please wait..."
     cd $RESOURCES_PATH
-    wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
-    echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
+    wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
+    echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
     apt-get update
     apt-get install -y mongodb-org
     # set mongodb runner to set default environment variables
+    # TODO: Workaround should not be used
     # alias mongod='LD_LIBRARY_PATH="" LD_PRELOAD="" /usr/bin/mongod'
-    mv /usr/bin/mongod /usr/bin/mongod-original
-    printf '#!/bin/bash\nbash -c "LD_LIBRARY_PATH= LD_PRELOAD= /usr/bin/mongod-original $*"' > /usr/bin/mongod
-    chmod a+rwx /usr/bin/mongod
+    # mv /usr/bin/mongod /usr/bin/mongod-original
+    # printf '#!/bin/bash\nbash -c "LD_LIBRARY_PATH= LD_PRELOAD= /usr/bin/mongod-original $*"' > /usr/bin/mongod
+    # chmod a+rwx /usr/bin/mongod
 else
     echo "MongoDB is already installed"
 fi
