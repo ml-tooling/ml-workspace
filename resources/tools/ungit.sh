5864ad80018bc6c2cfabca0fc77b0fd413c644d6
Lukas Masuch <lukas.masuch@gmail.com>
2020-12-09 22:09:14 +0000

    Update ungit version

--- resources/tools/ungit.sh
+++ resources/tools/ungit.sh
@@ -17,7 +17,7 @@ done
 if ! hash ungit 2>/dev/null; then
     echo "Installing Ungit. Please wait..."
     npm update
-    npm install -g ungit@1.5.13
+    npm install -g ungit@1.5.14
 else
     echo "Ungit is already installed"
 fi
